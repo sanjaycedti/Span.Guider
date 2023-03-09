c630291e5a4ba5dadad249f20b85408ae31698a9
Sanjay Sahani <sanjay.sahani@appinventiv.com>
2023-03-09 22:53:31 +0530

    first commit

---
+++ Span.Guider/GuiderBenchmarks.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace Span.Guider;
+
+[MemoryDiagnoser(false)]
+public class GuiderBenchmarks
+{
+	private static readonly Guid guid = Guid.Parse("ce905a43-18c2-45a4-a0c4-2e8ede2c4c40");
+	private const string str = "Q1qQzsIYpEWgxC6O3ixMQA";
+
+	[Benchmark]
+	public Guid ToGuidFromString()
+	{
+		return Guider.ToGuidFromString(str);
+	}
+
+    [Benchmark]
+    public Guid ToGuidFromStringOp()
+    {
+        return Guider.ToGuidFromStringOp(str);
+    }
+
+    [Benchmark]
+    public string ToStringFromGuid()
+	{
+		return Guider.ToStringFromGuid(guid);
+	}
+
+    [Benchmark]
+    public string ToStringFromGuidOp()
+    {
+        return Guider.ToStringFromGuidOp(guid);
+    }
+}
+
---
+++ Span.Guider/Program.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Running;
+using Span.Guider;
+
+BenchmarkRunner.Run<GuiderBenchmarks>();
+
+
+return;
+
+var id = Guid.NewGuid();
+
+Console.WriteLine(id);
+
+var base64Id = Convert.ToBase64String(id.ToByteArray());
+Console.WriteLine(base64Id);
+
+var urlFriendlyBase64 = Guider.ToStringFromGuid(id);
+Console.WriteLine(urlFriendlyBase64);
+
+var urlFriendlyBase64Op = Guider.ToStringFromGuidOp(id);
+Console.WriteLine(urlFriendlyBase64Op);
+
+
+var idAgain = Guider.ToGuidFromString(urlFriendlyBase64);
+Console.WriteLine(idAgain);
+
+var idAgainOp = Guider.ToGuidFromStringOp(urlFriendlyBase64);
+Console.WriteLine(idAgainOp);
